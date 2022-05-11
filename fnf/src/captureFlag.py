fbf0baa8cb91327d8c57b5d04aaa5a57d01e06a6
omarhmimy <90288844+omarhmimy@users.noreply.github.com>
2022-05-11 17:17:50 -0400

    wednesday changes

--- fnf/src/captureFlag.py
+++ fnf/src/captureFlag.py
@@ -1,22 +1,30 @@
+#!/usr/bin/python
 import cv2 
 import matplotlib.pyplot as plt
 import numpy as np
-import imutils
+#import imutils
+import rospy
+import sensor_msgs.msg
+from ackermann_msgs.msg import AckermannDriveStamped
 
 class car:
     def __init__ (self):
         #receive
-        self.sub = rospy.Subscriber("/csi_cam_0/imageraw", sensor_msgs.msg.Image, self.im_cb)
+        self.sub = rospy.Subscriber("/csi_cam_0/image_raw", sensor_msgs.msg.Image, self.im_cb)
         self.x = 0
          
         #send
         #see mushr_control for details
-        self.pub = rospy.Publisher(/vesc/mux/.../navigation, AckermanDriveStamped)
-        self.timer = rospy.Timer(rospy.Duration(0.1), self.pub_cb)
-          
+        self.pub = rospy.Publisher("/car/mux/ackermann_cmd_mux/input/navigation", AckermannDriveStamped)
+        self.timer = rospy.Timer(rospy.Duration(0.2), self.pub_cb)
+        self.c_locat = 0
+        self.ackermann_msg_id = 0
           
     def im_cb(self, img):
+        rospy.loginfo("hello")
 
+        # 75, 135,80   105,255,135  blue
+        #25,125,0   90,255,255  yellow
         #have to write seperate code that determines color and can set that as the flag color that we are trying to recognize
         f = open("flag_color.txt", "r")
         rgb_color = f.read()
@@ -28,10 +36,11 @@ class car:
 
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
+
         #lower = np.array([78,158,124])
         #upper = np.array([138,255,255])
         mask = cv2.inRange(hsv,lower,upper)
-        segment = cv2.bitwise_and(imge, imge, mask=mask)
+        segment = cv2.bitwise_and(img, img, mask=mask)
         contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         rect = cv2.minAreaRect(contours[0])
         x_coord = rect[0][0]
@@ -39,26 +48,29 @@ class car:
         #colormask & compound control
         # add way to determine if the color is on the left, right or in the middle
         # define this as -1, 0, 1 for left, straight, right respectively
-        self.c_locat = 0
         if x_coord < 400:
             self.c_locat = -1
         elif x_coord > 880:
             self.c_locat = 1
-        prp.publish(ctrl)
+        # self.pub.publish(ctrl)
        
     def pub_cb(self, event):
-        #calc control from self.x & self.if.Flag
-        self.pub.publish(ctrl)
+        rospy.loginfo("hello2")
 
-        if c_locat < 0:
+        #calc control from self.x & self.if.Flag
+        ctrl = [0, 0]
+        if self.c_locat < 0:
             ctrl[0] = 2
-            ctrl[1] = 0.2
-        elif c_locat > 0:
+            ctrl[1] = 1
+            rospy.loginfo("hello3")
+        elif self.c_locat > 0:
             ctrl[0] = 2
-            ctrl[1] = -0.2
+            ctrl[1] = -1
         else:
             ctrl[0] = 2
+            rospy.loginfo("hello4")
 
+        rospy.loginfo("hello4")
         assert len(ctrl) == 2
         ctrlmsg = AckermannDriveStamped()
         ctrlmsg.header.stamp = rospy.Time.now()
@@ -68,10 +80,16 @@ class car:
         self.pub.publish(ctrlmsg)
         self.ackermann_msg_id += 1
 
+        self.pub.publish(ctrlmsg)
 
 
 
 def main():
-    __init__node()
-  #  node = fastnfurious()
-  rospy.spin()
+    rospy.init_node("cptfnode")
+    rospy.loginfo("main()")
+    node = car()
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
+
--- fnf/src/colorRecognize.py
+++ fnf/src/colorRecognize.py
@@ -1,17 +1,22 @@
+#!/usr/bin/python
+import cv_bridge
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
+import sensor_msgs
+import rospy
 
 def main():
-    __init__node()
+    rospy.init_node("rnode")
 
-    
-    img = rospy.wait_for_message("/csi_cam_0/imageraw", sensor_msgs.msg.Image, timeout=None)
+    rospy.loginfo("hello")
+    img = rospy.wait_for_message("/csi_cam_0/image_raw", sensor_msgs.msg.Image, timeout=None)
 
     bridge = CvBridge()
     cv_image = bridge.imgmsg_to_cv2(img, desired_encoding='passthrough')
     color = cv_image[360,640]
 
+    rospy.loginfo("hello2")
     f = open("flag_color.txt", "w")
     f.write(color)
     f.close()
@@ -21,5 +26,6 @@ def main():
     #write that part into the im_cb function in captureFlag
 
 
-
+if __name__ == '__main__':
+    main()
 
